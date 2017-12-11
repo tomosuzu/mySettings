5aaa753539891e798c49135a115c98415f8856d2
tomosuzu <tomoya_su40@hotmail.com>
2017-12-11 17:35:31 +0900

    add .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+;;; キーバインド
+(define-key global-map (kbd "C-h") 'delete-backward-char) ; 削除
+(define-key global-map (kbd "M-?") 'help-for-help)        ; ヘルプ
+(define-key global-map (kbd "C-z") 'undo)                 ; undo
+(define-key global-map (kbd "C-c i") 'indent-region)      ; インデント
+(define-key global-map (kbd "C-c C-i") 'hippie-expand)    ; 補完
+(define-key global-map (kbd "C-c ;") 'comment-dwim)       ; コメントアウト
+(define-key global-map (kbd "C-o") 'toggle-input-method)  ; 日本語入力切替
+(define-key global-map (kbd "M-C-g") 'grep)               ; grep
+(define-key global-map (kbd "C-[ M-C-g") 'goto-line)      ; 指定行へ移動
